ae8b90267c4e2faaf2f43835cc98f6cfadcaa29f
pellincr <44884108+pellincr@users.noreply.github.com>
2019-12-11 22:36:04 -0500

    Login Screen
    Allows the user to login using their username and password

---
+++ Login.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+screen = Tk()
+screen.title("Login")
+screen.geometry("200x200")
+
+
+def run():
+    print("Logged in!")
+
+login_text = Label(text = "Login")
+register_text = Label(text = "Register")
+username_text = Label(text = "Username")
+password_text = Label(text = "Password")
+
+login_text.pack()
+username_text.place(x = 15, y = 50)
+password_text.place(x = 15, y = 95)
+
+
+login_button = Button(text = "login", fg = "white", bg = "black")
+register_button = Button(text = "register", fg = "white", bg = "black")
+
+login_button.place(x = 45, y = 150)
+register_button.place(x = 115, y = 150)
