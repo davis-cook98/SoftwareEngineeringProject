521f72cd2e4023d37adf0af6ecde3630e58e925c
Davis <daviscook@outlook.com>
2019-12-12 06:10:08 -0500

    Add null check for login

--- GetData/NewsAPICall.py
+++ GetData/NewsAPICall.py
@@ -7,6 +7,7 @@ from InsertData import jsonParse
 with open("../client_secret.json") as f:
     data = json.load(f)
 
+#URL for get request
 url = ('https://newsapi.org/v2/everything?'
         'q=College&'
         'q=University&'
@@ -16,13 +17,16 @@ url = ('https://newsapi.org/v2/everything?'
 
 response = requests.get(url)
 
+#Gets a nice name for a file
 now = datetime.datetime.now()
 dateString = str(now.year) + str(now.month) + str(now.day)
 
 jsonData = response.json()
 
+#Saves the output to json for processing
 with open("NewsFiles/" + dateString + ".json", "w") as dateFile:
     json.dump(jsonData, dateFile)
 # print(response.json())
 
+#Calls an aux function to insert the data
 jsonParse(dateString)
