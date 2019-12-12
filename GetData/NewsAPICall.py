b5d847e9a80c112965be9b2bd6b26d8d384db217
Davis <daviscook@outlook.com>
2019-12-12 05:02:57 -0500

    Update NewsAPICall

--- GetData/NewsAPICall.py
+++ GetData/NewsAPICall.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import os
+import datetime
 
 with open("../client_secret.json") as f:
     data = json.load(f)
@@ -14,5 +14,12 @@ url = ('https://newsapi.org/v2/everything?'
 
 response = requests.get(url)
 
-print(response.json())
+now = datetime.datetime.now()
+dateString = str(now.year) + str(now.month) + str(now.day)
+
+jsonData = response.json()
+
+with open("../NewsFiles/" + dateString + ".json", "w") as dateFile:
+    json.dump(jsonData, dateFile)
+# print(response.json())
     
