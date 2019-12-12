6dc8796654b4d92a7ee30e08ba4d6f620c00ffc8
pellincr <44884108+pellincr@users.noreply.github.com>
2019-12-11 23:13:00 -0500

    Update Login.py
    Input for username and password it clears the line for password once login is clicked

--- Login.py
+++ Login.py
@@ -5,8 +5,17 @@ screen.title("Login")
 screen.geometry("200x200")
 
 
-def run():
-    print("Logged in!")
+def run1():
+    new_text = "Logged in!"
+    print(new_text)
+    passw = password.get()
+    password_entry.delete(0,END)
+
+    
+def run2():
+    new_text = "Registered!"
+    print(new_text)
+    
 
 login_text = Label(text = "Login")
 register_text = Label(text = "Register")
@@ -17,9 +26,20 @@ login_text.pack()
 username_text.place(x = 15, y = 50)
 password_text.place(x = 15, y = 95)
 
+username = StringVar()
+username_entry = Entry(text = "Enter username", textvariable = username)
+username_entry.place(x = 73, y = 50)
 
-login_button = Button(text = "login", fg = "white", bg = "black")
-register_button = Button(text = "register", fg = "white", bg = "black")
+
+password = StringVar()
+password_entry = Entry(text = "Enter Password", textvariable = password)
+password_entry.place(x = 73, y = 95)
+
+
+login_button = Button(text = "login", fg = "white", bg = "black", command = run1)
+register_button = Button(text = "register", fg = "white", bg = "black", command = run2)
 
 login_button.place(x = 45, y = 150)
 register_button.place(x = 115, y = 150)
+
+screen.mainloop()
