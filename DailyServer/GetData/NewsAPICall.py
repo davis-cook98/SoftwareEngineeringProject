852634910570cf6b5eaf93972c96c9adfd9128ae
Davis <daviscook@outlook.com>
2020-03-24 22:13:17 -0400

    Update server

--- DailyServer/GetData/NewsAPICall.py
+++ DailyServer/GetData/NewsAPICall.py
@@ -33,29 +33,30 @@ def jsonParse(filename):
 
             ArtRepo.insert_one(insertArt)
 
-with open("../../client_secret.json") as f:
-    data = json.load(f)
+def InsertData():
+    with open("../../client_secret.json") as f:
+        data = json.load(f)
 
-#URL for get request
-url = ('https://newsapi.org/v2/everything?'
-        'q=College&'
-        'q=University&'
-        'q=HigherEducation&'
-        'SortBy=recent&'
-        'apiKey=' + data["NewsApi"])
+    #URL for get request
+    url = ('https://newsapi.org/v2/everything?'
+            'q=College&'
+            'q=University&'
+            'q=HigherEducation&'
+            'SortBy=recent&'
+            'apiKey=' + data["NewsApi"])
 
-response = requests.get(url)
+    response = requests.get(url)
 
-#Gets a nice name for a file
-now = datetime.datetime.now()
-dateString = str(now.year) + str(now.month) + str(now.day)
+    #Gets a nice name for a file
+    now = datetime.datetime.now()
+    dateString = str(now.year) + str(now.month) + str(now.day)
 
-jsonData = response.json()
+    jsonData = response.json()
 
-#Saves the output to json for processing
-#with open("NewsFiles/" + dateString + ".json", "w") as dateFile:
-#    json.dump(jsonData, dateFile)
-# print(response.json())
+    #Saves the output to json for processing
+    #with open("NewsFiles/" + dateString + ".json", "w") as dateFile:
+    #    json.dump(jsonData, dateFile)
+    # print(response.json())
 
-#Calls an aux function to insert the data
-jsonParse(jsonData)
+    #Calls an aux function to insert the data
+    jsonParse(jsonData)
--- DailyServer/Main.py
+++ DailyServer/Main.py
@@ -1,6 +1,43 @@
 import asyncio
+import aiocron
+import requests
+import json
+import datetime
+import pymongo
+import json
+import pprint
+from GetData.NewsAPICall import InsertData
+
+from pymongo import MongoClient
+
+#Connect to DB
+client = MongoClient('localhost', 27017)
+
+db = client.SoftwareEngineering
+
+ArtInfo = db.command("collstats", "ArtRepo")
+
+count = ArtInfo["count"]
 
 async def main():
+    #Get new Articles
+    @aiocron.crontab('1 0 * * *')
+    async def LoadFiles():
+        InsertData()
+
+    #Get article collection info
+    ArtInfo = db.command("collstats", "ArtRepo")
+
+    count = ArtInfo["count"]
+
+    #Clean up records
+    async def cleanDatabase():
+        if count > 20000:
+            limitSize = count - 20000
+            OldestRecords = db.ArtRepo.find().sort({ "date_time" : -1 }).limit(limitSize)
+
+            for record in OldestRecords:
+                db.ArtRepo.delete_one(record)
 
 
 asyncio.run(main())
