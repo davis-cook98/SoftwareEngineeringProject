cf63587de30103514c30dfb293c9b278c3ebfb96
rds526 <44884107+rds526@users.noreply.github.com>
2020-04-20 20:19:07 -0400

    Update endpoints.py

--- Read/endpoints.py
+++ Read/endpoints.py
@@ -12,30 +12,6 @@ CORS(app)
 def index():
  return json_response(Kudo(g.user).find_all_kudos())
 
-
-@app.route("/kudos", methods=["POST"])
-@login_required
-def create():
-   article_repo = ArticleSchema().load(json.loads(request.data))
-
-   if article_repo.errors:
-     return json_response({'error': article_repo.errors}, 422)
-
-   kudo = Kudo(g.user).create_kudo_for(article_repo)
-   return json_response(kudo)
-
-   @app.route("/kudos", methods=["POST"])
-   @login_required
-   def create():
-      user_repo = UserSchema().load(json.loads(request.data))
-
-      if user_repo.errors:
-        return json_response({'error': user_repo.errors}, 422)
-
-      kudo = Kudo(g.user).create_kudo_for(article_repo)
-      return json_response(kudo)
-
-
 @app.route("/kudo/<int:repo_id>", methods=["GET"])
 @login_required
 def show(repo_id):
@@ -45,46 +21,6 @@ def show(repo_id):
    return json_response(kudo)
  else:
    return json_response({'error': 'kudo not found'}, 404)
-
-
-@app.route("/kudo/<int:repo_id>", methods=["PUT"])
-@login_required
-def update(repo_id):
-   article_repo = ArticleSchema().load(json.loads(request.data))
-
-   if article_repo.errors:
-     return json_response({'error': article_repo.errors}, 422)
-
-   kudo_service = Kudo(g.user)
-   if kudo_service.update_kudo_with(repo_id, article_repo):
-     return json_response(article_repo.data)
-   else:
-     return json_response({'error': 'kudo not found'}, 404)
-
-     @app.route("/kudo/<int:repo_id>", methods=["PUT"])
-     @login_required
-     def update(repo_id):
-        user_repo = UserSchema().load(json.loads(request.data))
-
-        if user_repo.errors:
-          return json_response({'error': user_repo.errors}, 422)
-
-        kudo_service = Kudo(g.user)
-        if kudo_service.update_kudo_with(repo_id, user_repo):
-          return json_response(user_repo.data)
-        else:
-          return json_response({'error': 'kudo not found'}, 404)
-
-
-@app.route("/kudo/<int:repo_id>", methods=["DELETE"])
-@login_required
-def delete(repo_id):
- kudo_service = Kudo(g.user)
- if kudo_service.delete_kudo_for(repo_id):
-   return json_response({})
- else:
-   return json_response({'error': 'kudo not found'}, 404)
-
 
 def json_response(payload, status=200):
  return (json.dumps(payload), status, {'content-type': 'application/json'})
