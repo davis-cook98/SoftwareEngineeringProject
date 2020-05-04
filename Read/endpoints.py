277a4b0113a67db3c4eb27eeb4db63bad3f7f49b
AMauro26 <43528019+AMauro26@users.noreply.github.com>
2020-05-04 12:59:35 -0400

    endpoints updates

--- Read/endpoints.py
+++ Read/endpoints.py
@@ -9,7 +9,7 @@ import re
 
 #Connect to DB
 client = MongoClient('localhost', 27017)
-db = client.SoftwareEngineering 
+db = client.SoftwareEngineering
 ArtRepo = db.ArtRepo
 read = Flask(__name__)
 CORS(read)
@@ -25,6 +25,11 @@ def getAllArticles():
     title = request.args.get('title')
     return get_articles(title)
 
+@read.route('/ReadAPI/findUser/')
+def findUser():
+    title = request.args.get('title')
+    return
+
 
 #definitions
 def get_article2():
@@ -33,6 +38,9 @@ def get_article2():
 def get_article(title):
     query = re.compile("^" + title, re.IGNORECASE)
     return ArticleSchema().dump(ArtRepo.find_one({"Title": query}))
+
+
+def find_User(username, password):
     
 
 def get_articles(title):
@@ -44,10 +52,12 @@ def get_articles(title):
         query = re.compile("^" + title, re.IGNORECASE)
         for article in ArtRepo.find({"Title" : query},limit=5):
             allArticles.append(article)
-    
+
     results = ArticleSchema().dump(allArticles, many=True)
 
     return(jsonify(results))
+
+
 
 if __name__ == "__main__":
     read.run()
