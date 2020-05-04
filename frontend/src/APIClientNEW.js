214be85dcb6f7b1ba7374596eccfe34d28cdd78f
AMauro26 <43528019+AMauro26@users.noreply.github.com>
2020-05-04 04:15:45 -0400

    Update APIClientNEW.js

--- frontend/src/APIClientNEW.js
+++ frontend/src/APIClientNEW.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axios = require('axios').default;
 
-var config = { headers: {  
+var config = { headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*'}
   }
@@ -19,3 +19,43 @@ async function getOne(input) {
       console.log(error);
     }
   }
+
+  async function getAll(input) {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/ReadAPI/getAll/", {
+        params: {
+          title: input
+        }, config});
+        console.log(response);
+        }
+        catch (error) {
+          console.log(error);
+        }
+    }
+    async function addFav(input, input2) {
+      try {
+        const response = await axios.post("http://127.0.0.1:5001/WriteAPI/addFavorite/", {
+          params: {
+            title: input,
+            name: input2
+          }, config});
+          console.log(response);
+          }
+          catch (error) {
+            console.log(error);
+          }
+      }
+
+      async function removeFav(input, input2) {
+        try {
+          const response = await axios.post("http://127.0.0.1:5001/WriteAPI/removeFavorite/", {
+            params: {
+              title: input,
+              name: input2
+            }, config});
+            console.log(response);
+            }
+            catch (error) {
+              console.log(error);
+            }
+        }
