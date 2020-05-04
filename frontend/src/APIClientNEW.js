a543d256e1e67a8ceab7aa8b4225b474458553d8
Davis <daviscook@outlook.com>
2020-05-04 04:05:39 -0400

    Clarity

--- frontend/src/APIClientNEW.js
+++ frontend/src/APIClientNEW.js
@@ -3,18 +3,19 @@ import axios from 'axios';
 const axios = require('axios').default;
 
 var config = { headers: {  
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*'}
-}
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'}
+  }
 
 async function getOne(input) {
-  try {
-    const response = await axios.get("http://localhost:5000", { title: input }, config);
-    console.log(response);
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/ReadAPI/getOne/", {
+      params: {
+        title: input
+      }, config});
+      console.log(response);
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
-  catch (error) {
-    console.log(error);
-  }
-}
-
-getOne("c")
